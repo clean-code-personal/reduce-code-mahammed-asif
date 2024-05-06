ae417f401f7783ba77a289dea949a76255a9d861
Shaik, Mahammed Asif <mohammed.asif@philips.com>
2024-05-07 03:06:52 +0530

    Updated files

--- image.h
+++ image.h
@@ -1,20 +1,31 @@
 #pragma once
 #include <cstdint>
 #include <iostream>
+#include <functional>
 
 class Image {
 public:
 	Image(uint16_t rows, uint16_t columns): m_rows(rows), m_columns(columns) {
-		pixels = new uint8_t[m_rows * m_columns];
+		m_pixels = new uint8_t[m_rows * m_columns];
 	}
 	~Image() {
 		std::cout << "freeing pixel memory.\n";
-		delete[] pixels;
+		delete[] m_pixels;
 	}
-	bool imageSizeIsValid() {
+	bool imageSizeIsValid() const {
 		return m_rows <= 1024 && m_columns <= 1024;
 	}
+
+	void pixelRunner(std::function<uint8_t(uint8_t, int, int)> pixelProcessor) const {
+		for (int x = 0; x < m_rows; x++) {
+			for (int y = 0; y < m_columns; y++) {
+				int pixelIndex = x * m_columns + y;
+				m_pixels[pixelIndex] = pixelProcessor(m_pixels[pixelIndex], x, y);
+			}
+		}
+	}
+
 	const uint16_t m_rows;
 	const uint16_t m_columns;
-	uint8_t* pixels; // max 1k x 1k image
+	uint8_t* m_pixels; // max 1k x 1k image
 };
--- reduce-code.cpp
+++ reduce-code.cpp
@@ -4,7 +4,18 @@
 int main() {
 	auto image = std::make_shared<Image>(512, 512);
 	std::cout << "Brightening a 512 x 512 image\n";
-	ImageBrightener brightener(image);
-	int attenuatedCount = brightener.BrightenWholeImage();
-	std::cout << "Attenuated " << attenuatedCount << " pixels\n";
+
+	if (image->imageSizeIsValid()) {
+
+		ImageBrightener brightener(image);
+		int attenuatedCount = 0;
+		brightener.BrightenWholeImage(attenuatedCount);
+		std::cout << "Attenuated " << attenuatedCount << " pixels\n";
+	}
+	else {
+		std::cout << "Not a valid image... did nothing\n";
+		return 1;
+	}
+
+	
 }
